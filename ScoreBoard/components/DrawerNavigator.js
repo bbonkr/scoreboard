1b23a66dc52ad3f7a147e3cbb62c7dad320c555f
Pon Cheol Ku <bbon@bbon.kr>
2019-07-22 18:16:22 +0900

    UPDATED: - 서랍장 메뉴 Drawer menu 내용 추가 확인:

--- ScoreBoard/components/DrawerNavigator.js
+++ ScoreBoard/components/DrawerNavigator.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
-import { createDrawerNavigator } from 'react-navigation';
+import { View, Image, TouchableOpacity, ScrollView, Text } from 'react-native';
+import {
+    createDrawerNavigator,
+    SafeAreaView,
+    DrawerItems,
+} from 'react-navigation';
 
 import MainViewNavigator from './MainViewNavigator';
 import InfoViewNavigator from './InfoViewNavigator';
+import { HEADER_BACKGROUND_COLOR } from '../constants/colors';
 
-const DrawerNavigator = createDrawerNavigator({
-    Main: {
-        screen: MainViewNavigator,
-        navigationOptions: {
-            drawerLabel: 'Games',
-            title: 'Games',
+const DrawerNavigator = createDrawerNavigator(
+    {
+        Main: {
+            screen: MainViewNavigator,
+            navigationOptions: {
+                drawerLabel: 'Games',
+                title: 'Games',
+            },
+        },
+        Info: {
+            screen: InfoViewNavigator,
+            navigationOptions: {
+                drawerLabel: 'Info',
+                title: 'Info',
+            },
         },
     },
-    Info: {
-        screen: InfoViewNavigator,
-        navigationOptions: {
-            drawerLabel: 'Info',
-            title: 'Info',
-        },
+    {
+        contentComponent: props => (
+            <ScrollView>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View
+                        style={{
+                            backgroundColor: HEADER_BACKGROUND_COLOR,
+                            padding: 12,
+                        }}>
+                        <Text>ScoreBoard</Text>
+                    </View>
+                    <DrawerItems {...props} />
+                </SafeAreaView>
+            </ScrollView>
+        ),
     },
-});
+);
 
 export default DrawerNavigator;
