637319e8cc2260a392ba95885547cb5d41518e3a
Pon Cheol Ku <bbon@bbon.kr>
2019-07-22 14:38:56 +0900

    UPDATED: - 안드로이드 프로젝트 구성 수정     - realm - 안드로이드 실행 확인

--- ScoreBoard/android/app/src/main/java/com/scoreboard/MainApplication.java
+++ ScoreBoard/android/app/src/main/java/com/scoreboard/MainApplication.java
@@ -29,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      // new MainReactPackages();
+      // packages.add(new RealmReactPackage());
       return packages;
     }
 
